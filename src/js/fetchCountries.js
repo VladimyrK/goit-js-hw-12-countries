b5852fadb6b948f3572a47708bee14296cbb3a26
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2021-09-21 19:43:27 +0300

    Меняет способ обработки ошибки

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,15 @@
 export default function fetchCountries(searchQuery) {
   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('ЧТО-ТО НЕ ТАК');
+      if (!response.ok) {
+        throw new Error('нет ответа с сервера');
       }
+      return response.json();
     })
     .then(data => {
       return data;
+    })
+    .catch(error => {
+      console.log('ошибка');
     });
 }
