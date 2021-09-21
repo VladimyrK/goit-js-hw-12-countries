28b127516377fa5a8771839e8f590061fd95796e
VladimyrK <49499164+VladimyrK@users.noreply.github.com>
2021-09-21 16:44:09 +0000

    Deploying to gh-pages from @ VladimyrK/goit-js-hw-12-countries@b5852fadb6b948f3572a47708bee14296cbb3a26 🚀

--- src.dd1468fe.js
+++ src.dd1468fe.js
@@ -1,7 +1,7 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"irxn":[function(require,module,exports) {
-"use strict";function e(e){return fetch(`https://restcountries.eu/rest/v2/name/${e}`).then(e=>{if(200===e.status)return e.json();throw new Error("ЧТО-ТО НЕ ТАК")}).then(e=>e)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=e;
+"use strict";function e(e){return fetch(`https://restcountries.eu/rest/v2/name/${e}`).then(e=>{if(!e.ok)throw new Error("нет ответа с сервера");return e.json()}).then(e=>e).catch(e=>{console.log("ошибка")})}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=e;
 },{}],"pAws":[function(require,module,exports) {
 var define;
 var global = arguments[3];
@@ -26,4 +26,4 @@ var t=arguments[3],e="Expected a function",n=NaN,r="[object Symbol]",i=/^\s+|\s+
 },{"./fetchCountries.js":"irxn","../templates/country.hbs":"xAbl","@pnotify/core":"txz9","@pnotify/core/dist/PNotify.css":"AjW3","@pnotify/mobile":"ad6a","@pnotify/mobile/dist/PNotifyMobile.css":"AjW3","lodash.debounce":"PZFh"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/main.js");
 },{"./sass/main.scss":"clu1","./js/main.js":"d6sW"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-12-countries/src.8c5a292d.js.map
+//# sourceMappingURL=/goit-js-hw-12-countries/src.dd1468fe.js.map
